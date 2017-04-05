a2c38588d4fac679268de0bc20e0cd3e7c9dbc44
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2017-04-05 20:20:13 +0900

    test

---
+++ Yukicoder/test/3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+
+int N;
+int x[1000000];
+
+void solve()
+{
+}
+
+int main(){cin.tie(0);ios_base::sync_with_stdio(false);solve();return 0;}
